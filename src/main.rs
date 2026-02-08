c2ff50f6f125ccc9226e9413d932e75c79e2b0a8
Kamila Chyla <230475+kamchy@users.noreply.github.com>
2026-02-08 18:39:01 +0100

    Extract transform function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod anni;
+use std::num::Saturating;
+
 use anni::{Anniversary, DiaryErr};
 use chrono::{Local, NaiveDate};
 
@@ -7,18 +9,19 @@ fn today_date() -> NaiveDate {
 }
 
 fn main() {
-    let s = "2023-02-12 Foo bar baz";
+    let today = today_date();
+    println!("{}", transform("2023-02-12 Foo bar baz", &today));
+}
 
+fn transform(s: &str, today: &NaiveDate) -> String {
     match Anniversary::try_from(s) {
-        Ok(anni) => {
-            let today = today_date();
-            if let Some(diff) = anni.anni_diff(today) {
-                println!(
-                    "Today ({today}): {} days till {} anni of {}",
-                    diff.days_till, diff.which_anni, anni.description
-                )
-            };
-        }
-        Err(DiaryErr(info)) => println!("Error: {info} when parsing {s}"),
+        Ok(anni) => match anni.anni_diff(*today) {
+            Some(diff) => format!(
+                "Today ({today}): {} days till {} anni of [{}]",
+                diff.days_till, diff.which_anni, s
+            ),
+            None => format!("Could not find time delta for {s}"),
+        },
+        Err(DiaryErr(info)) => format!("Error: {info} when parsing {s}"),
     }
 }
